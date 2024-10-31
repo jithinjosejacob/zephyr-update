3402d15e362decef3063b220f098e6ae5f22b306
jithinjosejacob <42768797+jithinjosejacob@users.noreply.github.com>
2024-10-31 16:59:09 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,77 @@
-const utils = require("./utils/index.js");
+const axios = require("axios");
+
 const fs = require("fs");
+const crypto = require("crypto");
+
 const configs = JSON.parse(
   fs.readFileSync("./zephyr.config.json", "utf8").toString()
 );
 
-async function main() {
-  let testCycleKey = await utils.createTestCycle(configs.testCyclePrefix);
-  let testExecutions = await utils.getTestExecutionsFromJson(testCycleKey);
-  console.log(testExecutions);
-  for (let test of testExecutions) {
-    let res = await utils.createTestExecution(test);
-    console.log(res);
-  }
+async function createTestCycle(testCycleName) {
+  try {
+    const body = {
+      name: testCycleName + crypto.randomUUID().toString().slice(0, 3),
+      projectKey: configs.projectKey,
+    };
 
+    const uri = `${configs.endpoint}testcycles`;
+
+    const headers = {
+      Authorization: `Bearer ${configs.jwtToken}`,
+    };
+    let result = await axios.post(uri, body, { headers });
+
+    return result.data.key;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-main()
-  .then(() => {
-    console.log("done");
-  })
-  .catch((err) => {
+async function getTestExecutionsFromJson(testCycleKey) {
+  let json = JSON.parse(
+    fs.readFileSync("./test-results.json", "utf8").toString()
+  );
+  let testExecutions = [];
+  let suites = json.suites[0].suites[0];
+  for (let test of suites.specs) {
+    testExecutions.push({
+      testCaseKey: await getTestCaseKey(test.title),
+      statusName: test.ok ? "Pass" : "Fail",
+      projectKey: configs.projectKey,
+      testCycleKey: testCycleKey 
+
+    });
+  }
+  return testExecutions;
+}
+
+async function getTestCaseKey(testName) {
+  try {
+    const uri = `${configs.endpoint}testcases`;
+    const headers = {    
+      Authorization: `Bearer ${configs.jwtToken}`,
+    };
+
+    let result = await axios.get(uri, { headers });
+    return result.data.values.find((test) => test.name === testName).key;  
+   }
+    catch (err) {
+      console.log(err);
+    }   
+}
+
+async function createTestExecution(testExecution) {
+  try {
+    const uri = `${configs.endpoint}testexecutions`;
+    const headers = {
+      Authorization: `Bearer ${configs.jwtToken}`,
+    };
+
+    let result = await axios.post(uri, testExecution, { headers });
+    return result.data;
+  } catch (err) {
     console.log(err);
-  });
+  }
+}
+
+module.exports = { createTestCycle, getTestExecutionsFromJson, getTestCaseKey ,createTestExecution };
